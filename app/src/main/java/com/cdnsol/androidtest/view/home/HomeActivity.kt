339298629554a4491d018c458f10850c0101ed9a
nikitasolanki <nikitasolanki@cdnsol.com>
2022-06-14 14:37:47 +0530

    initiated

---
+++ app/src/main/java/com/cdnsol/androidtest/view/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.cdnsol.androidtest.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cdnsol.androidtest.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
