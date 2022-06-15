55089570ed03accf68a445f6f59ca921790e7929
nikitasolanki <nikitasolanki@cdnsol.com>
2022-06-15 14:43:49 +0530

    Recyclerview adapter with binding and binding adapters

--- app/src/main/java/com/cdnsol/androidtest/view/home/BindingAdapters.kt
+++ app/src/main/java/com/cdnsol/androidtest/view/home/BindingAdapters.kt
@@ -1,6 +1,8 @@
 package com.cdnsol.androidtest.view.home
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
@@ -9,4 +11,14 @@ fun ImageView.imageFromUrl(url: String?) {
     Glide.with(this.context)
         .load(url)
         .into(this)
+}
+
+@SuppressLint("ResourceAsColor")
+@BindingAdapter("colorText")
+fun TextView.colorText(color: Boolean?) {
+    if (color == true) {
+        this.setTextColor(resources.getColor(android.R.color.holo_blue_dark));
+    } else {
+        this.setTextColor(resources.getColor(android.R.color.black));
+    }
 }
--- app/src/main/java/com/cdnsol/androidtest/view/home/UsersListAdapter.kt
+++ app/src/main/java/com/cdnsol/androidtest/view/home/UsersListAdapter.kt
@@ -1,5 +1,6 @@
 package com.cdnsol.androidtest.view.home
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.cdnsol.androidtest.R
@@ -20,50 +22,35 @@ class UsersListAdapter(
     var context: Context,
     private var userlist: ArrayList<UserListData>
 ) : RecyclerView.Adapter<UsersListAdapter.ViewHolder>() {
-    private lateinit var binding: ItemUsersListBinding
+    //private lateinit var binding: ItemUsersListBinding
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = ItemUsersListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemUsersListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
 
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val userListData = userlist.get(position)
-        holder.tv_username.text = userListData.login
-        holder.container.setTag(userListData.login)
-        Glide.with(this.context)
-            .load(userListData.avatarUrl)
-            .into(holder.iv_user_img)
-        holder.container.setOnClickListener(View.OnClickListener { view ->
-            var tag : String = view.tag as String;
+        holder.holderBinding.userListData = userlist[position]
+        holder.holderBinding.container.setTag(R.id.id_position, position)
+        holder.holderBinding.container.setTag(R.id.id_data, userlist[position].login)
+        holder.holderBinding.container.setOnClickListener(View.OnClickListener { view ->
+            val position: Int = view.getTag(R.id.id_position) as Int
+            userlist[position].is_visited = true;
+            val tag: String = view.getTag(R.id.id_data) as String
             val intent = Intent(context, UserDetailActivity::class.java)
             intent.putExtra("LOGIN", tag)
-            context.startActivity(intent)
+            (context as Activity).startActivityForResult(intent, 99)
         })
     }
 
 
-    override fun getItemCount(): Int {
-        return userlist.size
-    }
+    override fun getItemCount() = userlist.size
 
     inner class ViewHolder(binding: ItemUsersListBinding) : RecyclerView.ViewHolder(binding.root) {
-        /*   fun bind(userListData: UserListData) {
-   //             binding.userListData  = userListData
-               binding.tvUserName.text = userListData.login
-           }*/
-        var tv_username: TextView
-        var iv_user_img: ImageView
-        var container : ConstraintLayout
-
-        init {
-            tv_username = itemView.findViewById(R.id.tv_user_name)
-            iv_user_img = itemView.findViewById(R.id.iv_user_img)
-            container =  itemView.findViewById(R.id.container)
-        }
-
+        val holderBinding = binding
     }
 
 
